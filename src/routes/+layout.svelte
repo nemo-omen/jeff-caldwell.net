a2c07d403f4c908e221adfe02368b9e8ff295546
Jeff Caldwell <nemo.omen@gmail.com>
2025-04-27 12:20:08 -0500

    Switch to sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '@picocss/pico';
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		children: Snippet;
+	};
+
+	const { children }: Props = $props();
+</script>
+
+<header></header>
+<main>
+	{@render children()}
+</main>
+<footer></footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Jeff Caldwell</h1>
